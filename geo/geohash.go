6fea4b1ab5240ce60470939bf66729a9f3f76161
liuyongshuai <liuyongshuai@didichuxing.com>
2018-04-19 19:20:36 +0800

    modify

--- geo/geohash.go
+++ geo/geohash.go
@@ -19,6 +19,7 @@ const (
 
 var (
 	bits      = []int{16, 8, 4, 2, 1}
+	bitsLen   = len(bits)
 	base32    = []byte(BASE32)
 	base32Pos = make(map[byte]int)
 )
@@ -68,6 +69,14 @@ func (sd *SquareDistrict) BorderLength() float64 {
 	precision:8		geoHash:wtte2qy9  	dist:32m x 32m
 	precision:9		geoHash:wtte2qy9m 	dist:4m x 4m
 	precision:10	geoHash:wtte2qy9m4	dist:1m x 1m
+计算方法：https://www.cnblogs.com/LBSer/p/3310455.html
+地球纬度区间是[-90,90]， 北京北海公园的纬度是39.928167，可以通过下面算法对纬度39.928167进行逼近编码:
+1）区间[-90,90]进行二分为[-90,0),[0,90]，称为左右区间，可以确定39.928167属于右区间[0,90]，给标记为1；
+2）接着将区间[0,90]进行二分为 [0,45),[45,90]，可以确定39.928167属于左区间 [0,45)，给标记为0；
+3）递归上述过程39.928167总是属于某个区间[a,b]。随着每次迭代区间[a,b]总在缩小，并越来越逼近39.928167；
+4）如果给定的纬度x（39.928167）属于左区间，则记录0，如果属于右区间则记录1，这样随着算法的进行会产生一个序列1011100，序列的长度跟给定的区间划分次数有关。
+通过上述计算，纬度产生的编码为10111 00011，经度产生的编码为11010 01011。偶数位放经度，奇数位放纬度，把2串编码组合生成新串：11100 11101 00100 01111。
+最后使用用0-9、b-z（去掉a, i, l, o）这32个字母进行base32编码，首先将11100 11101 00100 01111转成十进制，对应着28、29、4、15，十进制对应的编码就是wx4g。
 */
 func GeoHashEncode(lat, lng float64, precision int) (string, *SquareDistrict) {
 	if lat < MIN_LATITUDE || lat > MAX_LATITUDE {
@@ -99,7 +108,7 @@ func GeoHashEncode(lat, lng float64, precision int) (string, *SquareDistrict) {
 			}
 		}
 		isEven = !isEven
-		if bit < 4 {
+		if bit < bitsLen-1 {
 			bit++
 		} else {
 			buf.WriteByte(base32[ch])
@@ -123,7 +132,7 @@ func GeoHashDecode(geohash string) *SquareDistrict {
 		if !ok {
 			return ret
 		}
-		for i := 0; i < 5; i++ {
+		for i := 0; i < bitsLen; i++ {
 			ti := pos & bits[i]
 			if ti > 0 {
 				ti = 0
